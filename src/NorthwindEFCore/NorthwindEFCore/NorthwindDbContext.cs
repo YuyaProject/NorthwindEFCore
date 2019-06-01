1c17f10b8abf58b32672890b43b37e5ecdf78773
Alper Konuralp <alper@konuralp.gen.tr>
2019-06-01 20:09:48 +0300

    I fixed some codes.

--- src/NorthwindEFCore/NorthwindEFCore/NorthwindDbContext.cs
+++ src/NorthwindEFCore/NorthwindEFCore/NorthwindDbContext.cs
@@ -16,11 +16,14 @@ namespace NorthwindEFCore
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Customer> Customers { get; set; }
 		public DbSet<Employee> Employees { get; set; }
+		public DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<OrderDetail> OrderDetails { get; set; }
 		public DbSet<Product> Products { get; set; }
+		public DbSet<Region> Regions { get; set; }
 		public DbSet<Shipper> Shippers { get; set; }
 		public DbSet<Supplier> Suppliers { get; set; }
+		public DbSet<Territory> Territories { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
